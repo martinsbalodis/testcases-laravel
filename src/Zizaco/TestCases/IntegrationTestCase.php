22d7abaf57ea47dc51e5a2bc87c1e6d96eb2c49b
Martins Balodis <martins256@gmail.com>
2015-01-28 13:15:40 +0200

    do not print fuser errors

--- src/Zizaco/TestCases/IntegrationTestCase.php
+++ src/Zizaco/TestCases/IntegrationTestCase.php
@@ -207,7 +207,7 @@ class IntegrationTestCase extends \TestCase
 
         $artisan = $artisanDir."artisan";
         // before starting kill previous process if exists
-        exec("fuser -k 4443/tcp");
+        exec("fuser -k 4443/tcp > /dev/null 2>&1");
         $command = "php $artisan serve --port 4443";
         $outputPath = static::execAsyncAndWaitFor($command, 'development server started');
 
